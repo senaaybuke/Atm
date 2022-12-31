0499bc3a35d9aaa9a22d42568a6f8b9bab79ba2a
Sena Aybüke <s.aybukeaslan@gmail.com>
2022-12-31 12:50:52 +0100

    AtmSonVer

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,51 @@ public class Main {
     public static void main(String[] args) {
      Scanner scanner=new Scanner(System.in);
      int aktivbakiye = 1000;
-        System.out.println("Bakiyeniz =" +aktivbakiye);
-        System.out.println("Islemler");
-        System.out.println("1.Para Cekme");
-        System.out.println("2.Para Yatirma");
-        System.out.println("3.Bakiye Ogrenme");
-        System.out.println("Cikis");
+     boolean control = false;
+     while(!control){
 
-        System.out.println("isleminizi seciniz:");
-        int islem = scanner.nextInt();
-        switch(islem) {
-            case 1:
-                System.out.println("Bakiyeniz = " + aktivbakiye);
-                System.out.print("Çekmek istediğiniz tutarı giriniz: ");
-                int tutar = scanner.nextInt();
-                if(tutar > aktivbakiye) {
-                    System.out.print("Yetersiz bakiye Tekrar giriniz: ");
-                    tutar = scanner.nextInt();
-                }
-                aktivbakiye -= tutar;
+         System.out.println("Bakiyeniz =" +aktivbakiye);
+         System.out.println("Islemler");
+         System.out.println("1.Para Cekme");
+         System.out.println("2.Para Yatirma");
+         System.out.println("3.Bakiye Ogrenme");
+         System.out.println("4.Cikis");
+         System.out.println("isleminizi seciniz:");
+         int islem = scanner.nextInt();
 
-                System.out.println("Yeni bakiyeniz = " + aktivbakiye);
-                break;
-            case 2:
-                System.out.println("Bakiyeniz = " + aktivbakiye);
-                System.out.print("Yatırmak istediğiniz tutarı giriniz: ");
-                tutar = scanner.nextInt();
-                aktivbakiye += tutar;
-                System.out.println("Yeni bakiyeniz = " + aktivbakiye);
-                break;
-            case 3:
-                System.out.println("Bakiyeniz = " + aktivbakiye);
-                break;
-            case 4:
-                System.out.println("Cikis yaptiniz");
-            default:
-                System.out.println("Yanlıs islem secitiniz. Tekrar giriniz.");
-        }
-    }
+         switch(islem) {
+             case 1:
+                 System.out.println("Bakiyeniz = " + aktivbakiye);
+                 System.out.print("Çekmek istediğiniz tutarı giriniz: ");
+                 int tutar = scanner.nextInt();
+                 if(tutar > aktivbakiye) {
+                     System.out.print("Yetersiz bakiye Tekrar giriniz: ");
+                     tutar = scanner.nextInt();
+                 }
+                 aktivbakiye -= tutar;
+
+                 System.out.println("Yeni bakiyeniz = " + aktivbakiye);
+                 break;
+             case 2:
+                 System.out.println("Bakiyeniz = " + aktivbakiye);
+                 System.out.print("Yatırmak istediğiniz tutarı giriniz: ");
+                 tutar = scanner.nextInt();
+                 aktivbakiye += tutar;
+                 System.out.println("Yeni bakiyeniz = " + aktivbakiye);
+                 break;
+             case 3:
+                 System.out.println("Bakiyeniz = " + aktivbakiye);
+                 break;
+             case 4:
+                 System.out.println("Cikis yaptiniz");
+                 control = true;
+                 break;
+             default:
+                 System.out.println("Yanlıs islem secitiniz. Tekrar giriniz.");
+                 break;
+         }
+     }
+
+     }
+
     }
